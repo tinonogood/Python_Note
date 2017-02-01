d90af3f53f73f24e7081fd4c5a776d7f91f6d459
tino <tino5802@hotmail.com.tw>
2017-02-01 22:48:30 +0800

    add flask note

---
+++ flask/Install.md
@@ -0,0 +1,28 @@
+利用conda創虛擬環境
+
+$ conda create -n InteractiveWeb_flask python=3.5
+
+$ source activate InteractiveWeb_flask
+
+安裝
+
+$ conda install -n InteractiveWeb_flask flask
+
+測試
+
+$ flask: 顯示幫助文件
+
+$ vim hello.py`
+
+`from flask import Flask    
+app = Flask(__name__) 
+
+@app.route('/')      
+def hello_world():
+    return 'Hello World'`
+
+$ export FLASK_APP=hello.py
+
+$ export FLASK_DEBUG=1
+
+$ flask run
---
+++ flask/hiuser/hiuser.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for, request    
+from flask import render_template
+
+app = Flask(__name__) 
+
+@app.route('/', methods =["POST"])
+def hello_world():
+    return render_template("login.html")
+def login():
+    if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('hiuser',name = user))
+
+@app.route('/hiuser', methods =["GET"])
+def hiuser():
+    if request.method == 'GET':
+        name = request.args.get("name", "Error")
+        return render_template("hiuser.html", name = name)
