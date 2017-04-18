1644f8cf9c7ec563d31ea2a4136145c0e50f28c3
tino <tino5802@hotmail.com.tw>
2017-04-18 08:37:02 +0800

    add, add/del sql

--- flask/sqlite/sqlite_flask.py
+++ flask/sqlite/sqlite_flask.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 con = sql.connect('database.db')
 con.execute('DROP TABLE IF EXISTS members')
-con.execute('CREATE TABLE members (name TEXT, number TEXT)')
+con.execute('CREATE TABLE members (name TEXT, project TEXT)')
 
-@app.route('/')
+@app.route('/', methods= ['POST','GET'])
 def home():
 	con = sql.connect('database.db')
 	con.row_factory = sql.Row
 	cur = con.cursor()
-	cur.execute("select * from members")
-	rows = cur.fetchall();
+	cur.execute("SELECT * FROM members")
+	rows = cur.fetchall()
 	return render_template('home.html', rows = rows)
 
 @app.route('/addmember', methods= ['POST', 'GET'])
@@ -21,12 +21,12 @@ def addmember():
 	if request.method == 'POST':
 		try:
 			nm = request.form['nm']
-			nu = request.form['nu']
+			pj = request.form['pj']
 		
 #		with sql.connect('database.db') as con:
 			con = sql.connect('database.db')
 			cur = con.cursor()
-			cur.execute("INSERT INTO members (name, number) VALUES (?,?)", (nm, nu))
+			cur.execute("INSERT INTO members (name, project) VALUES (?,?)", (nm, pj))
 			con.commit()
 			msg = "Add Successfully"
 		except:
@@ -36,6 +36,24 @@ def addmember():
 		finally:
 			return render_template("result.html", msg = msg)
 
+@app.route('/delmember', methods= ['POST', 'GET'])
+def delmember():	
+	if request.method == 'POST':
+		try:
+			pj = request.form['pj']
+			con = sql.connect('database.db')
+			cur = con.cursor()
+			cur.execute("DELETE FROM members WHERE project = ?", pj)
+			con.commit()
+			msg_del = "Delete Successfully"
+		except:
+			con.rollback()
+			msg_del = "Error in delete operation"
+
+		finally:
+			return render_template("result.html", msg = msg_del)
+
 
 if __name__=='__main__':
 	app.run(debug = True) 
+
